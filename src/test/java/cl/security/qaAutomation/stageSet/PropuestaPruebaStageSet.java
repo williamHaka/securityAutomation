c6739bdde1b6c21882a2504fb9b9e85c92b8eb59
williamHaka <william.navarrete1990@gmail.com>
2019-06-18 12:55:29 -0400

    config jenkins

---
+++ src/test/java/cl/security/qaAutomation/stageSet/PropuestaPruebaStageSet.java
@@ -0,0 +1,15 @@
+package cl.security.qaAutomation.stageSet;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		tags = {"@NuevoAsegurado"}
+		,plugin = {"com.cucumber.listener.ExtentCucumberFormatter:ReporteHTML/FlujoCompassDPS.html" }
+)
+public class PropuestaPruebaStageSet extends StageSet{
+
+}
